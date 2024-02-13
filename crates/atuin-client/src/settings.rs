40b2f9cd552bcb3f959cb03a6f0c7ba2e7b2bbd7
Janne Heß <janne@hess.ooo>
2024-02-13 20:41:25 +0100

    feat(client): Allow sorting filter modes
    Closes #1417
    Closes #1654

--- crates/atuin-client/src/settings.rs
+++ crates/atuin-client/src/settings.rs
@@ -430,7 +430,8 @@ pub struct Settings {
     pub key_path: String,
     pub session_path: String,
     pub search_mode: SearchMode,
-    pub filter_mode: FilterMode,
+    pub filter_mode: Option<FilterMode>,
+    pub filter_modes: Vec<FilterMode>,
     pub filter_mode_shell_up_key_binding: Option<FilterMode>,
     pub search_mode_shell_up_key_binding: Option<SearchMode>,
     pub shell_up_key_binding: bool,
@@ -715,8 +716,14 @@ impl Settings {
             .set_default("sync_frequency", "10m")?
             .set_default("search_mode", "fuzzy")?
             .set_default("filter_mode", "global")?
-            .set_default("style", "compact")?
+            .set_default("style", "auto")?
+            .set_default("filter_mode", "global")?
             .set_default("inline_height", 40)?
+            .set_default("filter_mode", None::<String>)?
+            .set_default(
+                "filter_modes",
+                vec!["workspace", "global", "host", "session"],
+            )?
             .set_default("show_preview", true)?
             .set_default("preview.strategy", "auto")?
             .set_default("max_preview_height", 4)?
